51ffdaff5dfd560486fc28907feab629f10d0560
Cabralll13 <cabralpedro516@gmail.com>
2025-06-30 13:07:41 -0300

    Add files via upload

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,10 @@
+final = "O resultado é"
+x = 10
+y = 3.5
+z = x + y
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,6 @@
+juros = 1.01
+saldo = 500.0
+rendimento1 = saldo * juros
+rendimento2 = rendimento1 * juros
+print("Após 3 meses meu novo saldo é")
+print(rendimento2 * juros)
